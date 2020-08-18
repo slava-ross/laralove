4a11dfc8f6f9d9ef04c6a17842164255537b6846
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-08-18 21:58:18 +0400

    add project files

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title = $faker->realText(rand(10, 40));
+    $short_title = mb_strlen($title) > 30 ? mb_substr($title, 0, 30) . '...' : $title;
+    $created = $faker->dateTimeBetween('- 30 days', '-1 day');
+
+
+    return [
+        'title' => $title,
+        'short_title' => $short_title,
+        'author_id' => rand(1, 4),
+        'descr' => $faker->realText(rand(100, 500)),
+        'created_at' => $created,
+        'updated_at' => $created,
+    ];
+});
